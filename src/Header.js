cddfe3ba7ea1f0f0574ab18b0d74031ec800fa8b
David Rakosi <61084415+drakosi99@users.noreply.github.com>
2020-12-18 05:39:50 -0800

    ADD: session management

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import LocationOnIcon from '@material-ui/icons/LocationOn';
+import { auth, provider } from './firebase';
+
 
 function Header() {
+    const [userName, setUserName] = useState();
+
+    const signIn = () => {
+        // sign in with google
+        auth.signInWithPopup(provider)
+            .then((result) => {
+                console.log(result)
+                setUserName(result.user.displayName)
+            })
+            .catch((error) => alert(error.message))
+    }
+
+    const handleSignOut = () => {
+        // sign out
+        auth.signOut().then(() => {
+            setUserName()
+        }).catch((error) => alert(error.message))
+    }
+
+    console.log(userName) // username stored by the app
+    console.log(auth.currentUser) // firebase session management status 
+
     return (
         <div className="Header">
             <div className="Header-logo">
-                <img src={process.env.PUBLIC_URL + '/logo.png'}/>
+                <img src={process.env.PUBLIC_URL + '/logo.png'} />
             </div>
 
             <div className="Header-optionAddress">
@@ -20,7 +44,7 @@ function Header() {
             </div>
 
             <div className="Header-search">
-                <input className="Header-searchInput" type="text"/>
+                <input className="Header-searchInput" type="text" />
                 <div className="Header-searchIconContainer">
                     <SearchIcon className="Header-searchIcon" />
                 </div>
@@ -28,8 +52,17 @@ function Header() {
 
             <div className="Header-navItems">
                 <div className="Header-option">
-                    <span className="Header-optionLineOne">Hello, Nazariy</span>
-                    <span className="Header-optionLineTwo">Account & Lists</span>
+                    {/* deciding if sing in or sign out button is shown */}
+                    {
+                        userName ? (
+                            <div className='Header-signOut' onClick={handleSignOut}>
+                                <div className="Header-optionLineOne">Hello, {userName}</div>
+                                <div className="Header-optionLineTwo">Account & Lists</div>
+                            </div>
+                        ) : (
+                                <button onClick={signIn}>Sign In</button>
+                            )
+                    }
                 </div>
 
                 <div className="Header-option">
